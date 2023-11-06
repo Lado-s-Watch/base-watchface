23d9dbccb0565e7fa29f11cd92ee0e33889be476
huyhn <huyhn@hybrid-technologies.vn>
2023-11-07 00:29:39 +0700

    usestyle compose

---
+++ core/ui-wear/src/main/java/com/dotsdev/basewatchface/ui/wear/WatchFaceConfigState.kt
@@ -0,0 +1,102 @@
+package com.dotsdev.basewatchface.ui.wear
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.wear.watchface.editor.EditorSession
+import com.dotsdev.basewatchface.ui.wear.ext.createWatchFacePreview
+import com.dotsdev.basewatchface.ui.wear.ext.getColorStyle
+import com.dotsdev.basewatchface.ui.wear.ext.getMinuteHandStyle
+import com.dotsdev.basewatchface.ui.wear.ext.getShowComplicationsInAmbient
+import com.dotsdev.basewatchface.ui.wear.ext.getTickEnabledStyle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.plus
+import kotlinx.coroutines.yield
+
+interface WatchFaceConfigState {
+    val editWatchFaceUiState: EditWatchFaceUiState
+    var userStyles: UserStyles
+    suspend fun update()
+}
+
+@Composable
+fun rememberWatchFaceConfigState(
+    editorSession: EditorSession
+): WatchFaceConfigState {
+    val scope = rememberCoroutineScope()
+    var editWatchFaceUiState: EditWatchFaceUiState by remember {
+        mutableStateOf(EditWatchFaceUiState.Loading("initial"))
+    }
+    return rememberSaveable(editorSession) {
+        object : WatchFaceConfigState {
+            override val editWatchFaceUiState: EditWatchFaceUiState
+                get() = editWatchFaceUiState
+
+            override var userStyles: UserStyles by mutableStateOf(
+                with(editorSession.userStyleSchema) {
+                    val showComplicationsInAmbientStyle =
+                        editorSession.userStyle.value.getShowComplicationsInAmbient()
+                    val colorStyle = editorSession.userStyle.value.getColorStyle()
+                    val ticksEnabledStyle = editorSession.userStyle.value.getTickEnabledStyle()
+                    val minuteHandStyle = editorSession.userStyle.value.getMinuteHandStyle()
+                    UserStyles(
+                        showComplicationsInAmbient = showComplicationsInAmbientStyle,
+                        colorStyleId = colorStyle,
+                        ticksEnabled = ticksEnabledStyle,
+                        minuteHandLength = (minuteHandStyle * 1000f).toFloat()
+                    )
+                },
+            )
+
+            override suspend fun update(): Unit = coroutineScope {
+                combine(
+                    editorSession.userStyle,
+                    editorSession.complicationsPreviewData
+                ) { userStyle, complicationsPreviewData ->
+                    yield()
+                    with(editorSession.userStyleSchema) {
+                        editWatchFaceUiState = EditWatchFaceUiState.Success(
+                            editorSession.createWatchFacePreview(
+                                complicationsPreviewData
+                            )
+                        )
+                    }
+                }.launchIn(scope + Dispatchers.Main.immediate)
+
+                snapshotFlow { userStyles }.distinctUntilChanged().onEach {
+                    editorSession.userStyle.value =
+                        editorSession.userStyle.value.toMutableUserStyle().apply {
+                            with(editorSession.userStyleSchema) {
+                                // TODO: set UserStyle
+                            }
+                        }.toUserStyle()
+                }.launchIn(scope)
+            }
+        }
+    }
+}
+
+sealed class EditWatchFaceUiState {
+    data class Success(val imageBitmap: ImageBitmap) : EditWatchFaceUiState()
+    data class Loading(val message: String) : EditWatchFaceUiState()
+    data class Error(val exception: Throwable) : EditWatchFaceUiState()
+}
+
+data class UserStyles(
+    val colorStyleId: String,
+    val ticksEnabled: Boolean,
+    val minuteHandLength: Float,
+    val showComplicationsInAmbient: Boolean,
+)
+
--- core/ui-wear/src/main/java/com/dotsdev/basewatchface/ui/wear/ext/UserStyleExt.kt
+++ core/ui-wear/src/main/java/com/dotsdev/basewatchface/ui/wear/ext/UserStyleExt.kt
@@ -1,9 +1,17 @@
 package com.dotsdev.basewatchface.ui.wear.ext
 
+import android.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.wear.watchface.DrawMode
+import androidx.wear.watchface.RenderParameters
+import androidx.wear.watchface.complications.data.ComplicationData
+import androidx.wear.watchface.editor.EditorSession
 import androidx.wear.watchface.style.ExperimentalHierarchicalStyle
 import androidx.wear.watchface.style.UserStyle
 import androidx.wear.watchface.style.UserStyleSchema
 import androidx.wear.watchface.style.UserStyleSetting
+import androidx.wear.watchface.style.WatchFaceLayer
 
 context(UserStyleSchema)
 @OptIn(ExperimentalHierarchicalStyle::class)
@@ -13,4 +21,53 @@ fun UserStyle.getShowComplicationsInAmbient(): Boolean {
             as UserStyleSetting.BooleanUserStyleSetting
     return (this[showComplicationsInAmbientSetting]
             as UserStyleSetting.BooleanUserStyleSetting.BooleanOption).value
+}
+
+context(UserStyleSchema)
+@OptIn(ExperimentalHierarchicalStyle::class)
+fun UserStyle.getColorStyle(): String {
+    val id = UserStyleSetting.Id("color_style")
+    val colorStyleSetting = rootUserStyleSettings.find { it.id == id }
+            as UserStyleSetting.ListUserStyleSetting
+    return (this[colorStyleSetting]
+            as UserStyleSetting.ListUserStyleSetting.ListOption).id.toString()
+}
+
+context(UserStyleSchema)
+@OptIn(ExperimentalHierarchicalStyle::class)
+fun UserStyle.getTickEnabledStyle(): Boolean {
+    val id = UserStyleSetting.Id("tick_enabled_style")
+    val styleSetting = rootUserStyleSettings.find { it.id == id }
+            as UserStyleSetting.BooleanUserStyleSetting
+    return (this[styleSetting]
+            as UserStyleSetting.BooleanUserStyleSetting.BooleanOption).value
+}
+
+context(UserStyleSchema)
+@OptIn(ExperimentalHierarchicalStyle::class)
+fun UserStyle.getMinuteHandStyle(): Double {
+    val id = UserStyleSetting.Id("minute_hand_style")
+    val styleSetting = rootUserStyleSettings.find { it.id == id }
+            as UserStyleSetting.DoubleRangeUserStyleSetting
+    return (this[styleSetting]
+            as UserStyleSetting.DoubleRangeUserStyleSetting.DoubleRangeOption).value
+}
+
+context(UserStyleSchema)
+fun EditorSession.createWatchFacePreview(
+    complicationsPreviewData: Map<Int, ComplicationData>
+): ImageBitmap {
+    return this.renderWatchFaceToBitmap(
+        RenderParameters(
+            DrawMode.INTERACTIVE,
+            WatchFaceLayer.ALL_WATCH_FACE_LAYERS,
+            RenderParameters.HighlightLayer(
+                RenderParameters.HighlightedElement.AllComplicationSlots,
+                Color.RED, // Red complication highlight.
+                Color.argb(128, 0, 0, 0) // Darken everything else.
+            )
+        ),
+        this.previewReferenceInstant,
+        complicationsPreviewData
+    ).asImageBitmap()
 }
